d53a3904370ef490889b8cbce28e451c3aeb48c1
leeyoungseung <rudnfskf2@gmail.com>
2021-09-12 00:08:31 +0900

    feat:[File] ReadFile

---
+++ src/main/java/template/java/file/File_02_Read.java
@@ -0,0 +1,28 @@
+package template.java.file;
+
+import java.io.File;
+import java.util.List;
+
+import template.java.utils.FileUtil;
+
+public class File_02_Read {
+
+	public static void main(String[] args) {
+		
+		try {
+			File target = new File(args[0]);
+			FileUtil fu = new FileUtil();
+			List<String> list = fu.readPerLine(target);
+			
+			list.forEach(res -> {
+				System.out.println("File Contents : "+ res);
+			});
+			
+			System.exit(0);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+}
