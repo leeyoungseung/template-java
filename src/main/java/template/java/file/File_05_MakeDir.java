c75766931e1cb683c9263a05ed421ff60a9b23e8
leeyoungseung <rudnfskf2@gmail.com>
2021-09-12 09:27:46 +0900

    feat:[File] Make Directory

---
+++ src/main/java/template/java/file/File_05_MakeDir.java
@@ -0,0 +1,26 @@
+package template.java.file;
+
+import template.java.utils.FileUtil;
+
+public class File_05_MakeDir {
+	
+	public static void main(String[] args) {
+		try {
+			for (String str : args ) {
+				System.out.println("Param : "+str);
+			}
+
+			String path = args[0];
+			FileUtil fu = new FileUtil();
+			
+			System.out.println("Case 1 : "+ fu.makeDir(path));
+			
+			System.exit(0);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+}
