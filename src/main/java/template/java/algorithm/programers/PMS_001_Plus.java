806d5608eab6fd5c1404b9a9e91a32b61bcca44f
leeyoungseung <rudnfskf2@gmail.com>
2022-12-24 02:26:35 +0900

    프로그래머스 코딩테스트 입문 day1

---
+++ src/main/java/template/java/algorithm/programers/PMS_001_Plus.java
@@ -0,0 +1,13 @@
+package template.java.algorithm.programers;
+
+public class PMS_001_Plus {
+
+	public static void main(String[] args) {
+		System.out.println(solution(3, 4));
+	}
+
+    public static int solution(int num1, int num2) {
+        int answer = num1 + num2;
+        return answer;
+    }
+}
---
+++ src/main/java/template/java/algorithm/programers/PMS_003_Multiply.java
@@ -0,0 +1,13 @@
+package template.java.algorithm.programers;
+
+public class PMS_003_Multiply {
+
+	public static void main(String[] args) {
+		System.out.println(solution(3, 4));
+	}
+
+    public static int solution(int num1, int num2) {
+        int answer = num1 * num2;
+        return answer;
+    }
+}
